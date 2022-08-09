996f60da02ad654d3c4594be225d3663e929c0f0
syifasri <110007883+syifasri@users.noreply.github.com>
2022-08-09 11:57:41 +0700

    Add files via upload

---
+++ tambah2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get"action="proses_tambah2.php">
+    <p> name<input name="nama" type="text"></p>
+    <p> kelas<input name="kelas" type="text"></p>
+    <button type="submit">simpan</button></form>''
+</body>
+</html>
